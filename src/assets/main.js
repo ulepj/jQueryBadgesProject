7e8935bf12099f8fe025b07337e2b8368a15fb16
ulepj <jerome_ulep@jeromeulepsMBP.hawaii.rr.com>
2017-02-05 21:39:02 -1000

    part 3
    part 3

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,36 +1,5 @@
 $(function() {
 
-  $.ajax({
-    url: 'https://www.codeschool.com/users/jerome_22.json',
-    dataType: 'jsonp',
-    success: function(response) {
-      console.log('response', response.courses.completed);
-    }
-  });
-  function addCourses(courses){
+  // your code will go here
 
-    var $badges = $('#badges');
-
-    courses.forEach(function(course) {
-
-      var $course = ('<div/>'),{
-        'class': 'course'
-      }).appendTo($badges);
-
-      $('<h3 />', {
-        text: course.title
-      }).appendTo($course);
-
-      $('<img />', {
-        src: course.badge
-      }).appendTo($course);
-
-      $('<a />', {
-        'class': 'btn btn-primary',
-        target: '_blank',
-        href: course.url,
-        text: 'See Course'
-      }).appendTo($course);
-    })
-  }
 });
